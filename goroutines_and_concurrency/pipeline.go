633a9820916a23a7c58a9e1ca3b513343489cda5
Twofold-One <evan_geras@mail.ru>
2022-03-25 23:11:41 +0700

    goroutines_and_concurrency/piplene example added

---
+++ goroutines_and_concurrency/pipeline.go
@@ -0,0 +1,41 @@
+// From the book: "Get programming with go". Youngman N., Peppe R.
+// "A gopher assembly line" example which covers following topics:
+// Start a goroutine
+// Use channels to communicate
+// Understand channel pipelines
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func sourceGopher(downstream chan string) {
+	for _, v := range []string{"hello world", "a bad apple", "goodbye all"} {
+		downstream <- v
+	}
+	close(downstream)
+}
+
+func filterGopher(upstream, downstream chan string) {
+	for item := range upstream {
+		if !strings.Contains(item, "bad") {
+			downstream <- item
+		}
+	}
+	close(downstream)
+}
+
+func printGopher(upstream chan string) {
+	for v := range upstream {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	c0 := make(chan string)
+	c1 := make(chan string)
+	go sourceGopher(c0)
+	go filterGopher(c0, c1)
+	printGopher(c1)
+}
