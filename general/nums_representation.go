17efccb47b7e3b33886b3a299067d944740d50da
Twofold-One <evan_geras@mail.ru>
2022-03-27 12:57:21 +0700

    added general and network prog examples

---
+++ general/nums_representation.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 12345
+
+	fmt.Printf("Decimal: %d\n", n)
+	fmt.Printf("Binary: %b\n", n)
+	fmt.Printf("Ocatal: %o\n", n)
+	fmt.Printf("Hexadecimal: %x\n", n)
+}
