45e522ef54456088f14256a8d05d64b908a76229
Twofold-One <evan_geras@mail.ru>
2022-05-08 23:34:11 +0700

    time/date examples finished

--- basic/datesAndTime.go
+++ basic/datesAndTime.go
@@ -42,5 +42,55 @@ func DatesAndTimeExample() {
 	}
 	fmt.Println(t)
 
-	
+	// get the time elapsed between two points using method Sub
+	start := time.Now()
+	time.Sleep(1 * time.Second)
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Println(elapsed)
+
+	// check if time is between two points
+	location, err := time.LoadLocation("UTC")
+	if err != nil {
+		panic(err)
+	}
+	firstJanuary1980 := time.Date(1980, 1, 1, 0, 0, 0, 0, location)
+
+	timeToParse = "2019-02-15T07:33-02:00"
+	t1, err := time.Parse("2006-01-02T15:04-07:00", timeToParse)
+	if err != nil {
+		panic(err)
+	}
+
+	now = time.Now()
+	if t1.After(firstJanuary1980) && t1.Before(now) {
+		fmt.Println(t, " is between", firstJanuary1980, " and ", now)
+	} else {
+		fmt.Println("not between")
+	}
+
+	// adding days, hours, minutes and seconds to a time
+	now = time.Now()
+	fmt.Println(now)
+
+	// +1 year, 6 months and 10 days
+	now = now.AddDate(1, 6, 10)
+	fmt.Println(now)
+
+	now.Add(time.Second * 5)
+	now.Add(time.Minute * 5)
+	now.Add(time.Hour * 5)
+
+	// iterate over time
+	st, err := time.Parse("2006-01-02", "2019-02-19")
+	if err != nil {
+		panic(err)
+	}
+	en, err := time.Parse("2006-01-02", "2020-07-17")
+	if err != nil {
+		panic(err)
+	}
+	for i := st; i.Unix() < en.Unix(); i = i.AddDate(0, 0, 1) {
+		fmt.Println(i.Format(time.RFC3339))
+	}
 }
