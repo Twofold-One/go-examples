571c67e7f5107c52cac4effa458c0f402b949cd2
Twofold-One <evan_geras@mail.ru>
2022-05-10 14:01:33 +0700

    mongodb example added

--- basic/datastorage/mongodb/mongodb.go
+++ basic/datastorage/mongodb/mongodb.go
@@ -2,8 +2,10 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -14,7 +16,7 @@ func MongodbExample() {
 	// create mongo.Client with Connect function
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:secret@localhost:27017/person"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 
 	// defer call to disconnect
 	defer func() {
@@ -28,26 +30,84 @@ func MongodbExample() {
 	defer cancel()
 	err = client.Ping(ctx, readpref.Primary())
 
-	// TODO
+	
+	collection := client.Database("db").Collection("person")
 
-	// // create the client
-	// client, err := mongo.NewClient(`mongodb://root:secret@localhost:27017/person`)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// insert document to collection
+	// insertDocument(collection)
 
-	// // connect to db
-	// ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
-	// defer cancel()
-	// err = client.Connect(ctx)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// read document from collection
+	readDocument(collection)
 
-	// // ping the server
-	// err = client.Ping(ctx, nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// logical query
+	logicalDocumentQuery(collection)
 
+	// update document
+	updateDocument(collection)
+}
+
+// insert document
+func insertDocument(collection *mongo.Collection) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := collection.InsertOne(ctx, bson.D{{"name", "Jhon"}, {"lastname", "Doe"}})
+	if err != nil {
+		panic(err)
+	}
+	id := res.InsertedID
+	fmt.Println(id)
+}
+
+// read a document
+type Person struct {
+	Name string
+	Lastname string
+}
+
+func readDocument(collection *mongo.Collection) {
+	filter := bson.D{{"name", "Peter"}}
+	result := Person{}
+	err := collection.FindOne(context.Background(), filter).Decode(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", result)
+}
+
+// query document with logical selector
+func logicalDocumentQuery(collection *mongo.Collection) { 
+	q1 := bson.M{"name": bson.M{"$eq": "Jhon"}}
+	q2 := bson.M{"lastname": bson.M{"$eq": "Doe"}}
+	clauses := bson.A{q1, q2}
+	filter := bson.M{"$and": clauses}
+	cur, err := collection.Find(context.Background(), filter)
+	if err != nil {
+		panic(err)
+	}
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		var result bson.D
+		err := cur.Decode(&result)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(result)
+	}
+	if err := cur.Err(); err != nil {
+		panic(err)
+	}
+}
+
+// update a document
+func updateDocument(collection *mongo.Collection) {
+	filter := bson.M{"name": "Jhon"}
+	update := bson.M{"$set": bson.M{"name": "John"}}
+	
+	res := collection.FindOneAndUpdate(context.Background(), filter, update)
+	resDecoded := Person{}
+	err := res.Decode(&resDecoded)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v", resDecoded)
 }
